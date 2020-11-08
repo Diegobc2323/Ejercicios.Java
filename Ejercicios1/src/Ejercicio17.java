d750cf9d9b0224b7bdc1b16f7d0a8527469ea9fb
Diego <alum22cassasdiego@cpifpbajoaragon.com>
2020-11-08 17:22:06 +0100

    Ejercicio 17 terminado y ejercicio 7 del 1.2 duda

--- Ejercicios1/src/Ejercicio17.java
+++ Ejercicios1/src/Ejercicio17.java
@@ -6,9 +6,32 @@ public class Ejercicio17 {
 		// Ejercicio 17
 		
 		Scanner leer = new Scanner(System.in);
-
 		
+		int num, min = 99999999, max=0, suma=0, contador=0;
 		
+		System.out.println("Dime una serie de numeros y te dare una serie de informacion cuando teclees el numero 0");
+		num = leer.nextInt();
+		
+		while (num!=0) {
+			
+			if (min>num) {
+				min=num;
+			}
+			
+			if (max<num) {
+				max=num;
+			}
+			
+			suma += num;
+			
+			contador++;
+			
+			num = leer.nextInt();
+		}
+		
+		System.out.println("De los numeros que me has dado "+min+" es el numero mas pequeño");
+		System.out.println("De los numeros que me has dado "+max+" es el numero mas grande");
+		System.out.println("La media de tus numeros es "+(suma/contador));
 		
 		
 	}
---
+++ Ejercicios1/src/Ejercicio7_1_2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ejercicio7_1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner leer = new Scanner(System.in);
+		
+		int suma_pares = 0, suma_impares = 0;
+		
+		
+		for (int i = 1; i == 100; i++) {
+			
+			if (i%2==0) {
+				
+				suma_pares = suma_pares+i;
+				
+			} else {
+				
+				suma_impares += suma_impares+i;
+				
+			}
+			
+		}
+		
+		
+		System.out.println("La suma de los numeros pares hasta el 100 es "+suma_pares);
+		System.out.println("La suma de los numeros impares hasta el 100 es "+suma_impares);
+		
+	}
+
+}
